09f9a8fd182d44bfa05c0ce945c4bdf9a0e4eb60
vectorxz <vectorx2263@hotmail.com>
2021-03-03 14:16:53 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,36 @@ export default function Home() {
         </div>
       </main>
 
+      <main className="container m-auto">
+        <div className="justify-center">
+          <div className="grid lg:grid-cols-6 grid-cols-1 gap-y-3">
+            <div className="lg:col-span-3 col-auto flex self-start lg:self-end justify-center pt-8 lg:pt-0">
+              <h1 className="lg:text-6xl text-3xl font-bold">See me on</h1>
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-6 grid-cols-2 gap-y-3 my-3">
+            <div className="lg:col-span-2 col-span-1 flex lg:justify-end justify-center">
+              <a href="https://www.linkedin.com/in/patipholpus/" className={style.platformLink}>LinkedIn</a>
+            </div>
+            <div className="lg:col-span-2 col-span-1 flex justify-center">
+              <a href="https://github.com/VectorXz" className={style.platformLink}>GitHub</a>
+            </div>
+            <div className="lg:col-span-2 col-span-1 flex lg:justify-start justify-center">
+              <a href="https://dev.to/vectorxz" className={style.platformLink}>Dev.to</a>
+            </div>
+            <div className="lg:col-span-2 col-span-1 flex lg:justify-end justify-center">
+              <a href="https://vectorxz.medium.com/" className={style.platformLink}>Medium</a>
+            </div>
+            <div className="lg:col-span-2 col-span-1 flex justify-center">
+              <a href="https://www.codecademy.com/profiles/vectorx2263" className={style.platformLink}>Codecademy</a>
+            </div>
+            <div className="lg:col-span-2 col-span-1 flex lg:justify-start justify-center">
+              <a href="https://www.hackerrank.com/vectorxz2263" className={style.platformLink}>Hackerrank</a>
+            </div>
+          </div>
+        </div>
+      </main>
+
       <footer className="container mx-auto pb-2">
         <div className="grid text-xs">
           <div className="flex justify-center text-center">
